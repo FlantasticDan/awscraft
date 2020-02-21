7649fc807caf81a7f8f60addf5ed8c0a9926f126
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-02-20 21:57:29 -0800

    Debugging Auto Installer

--- AWS Instance/AWScraft_Installer.sh
+++ AWS Instance/AWScraft_Installer.sh
@@ -7,21 +7,17 @@ echo "##     ## ##  ##  ##  ######  ##       ########  ##     ## ######      ## 
 echo "######### ##  ##  ##       ## ##       ##   ##   ######### ##          ##    "
 echo "##     ## ##  ##  ## ##    ## ##    ## ##    ##  ##     ## ##          ##    "
 echo "##     ##  ###  ###   ######   ######  ##     ## ##     ## ##          ##    "
-echo "                              Installer v.0.1                                "
+echo "                              Installer v.0.2                                "
 
 echo "Installing Python"
-sudo yum install python3
+sudo yum -y install python3
+sudo yum -y install python3-pip
 
 echo "Python Installed, Installing Java"
-sudo yum install java
+sudo yum -y install java
 
-echo "Java Installed, Installing pip"
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-rm get-pip.py
-
-echo "pip Installed, Installing Python Dependencies"
-pip install websockets, requests
+echo "Java Installed, Installing Python Dependencies"
+pip3 install websockets requests
 
 echo "Dependencies Installed, Fetching AWScraft Resources"
 mkdir awscraft
